0889d20b02ebb88c710632f0c6ece06a01353a96
kjbowman00 <kjbowman00@vt.edu>
2019-04-27 01:50:29 -0400

    Initial Commit

---
+++ JuiceDungeon/src/juice/GameRunner.java
@@ -0,0 +1,28 @@
+/*
+ * Virginia Tech Honor Code Pledge:
+ *
+ * As a Hokie, I will conduct myself with honor
+ * and integrity at all times.
+ * I will not lie, cheat, or steal, nor will I
+ * accept the actions of those who do.
+ * -- Kyle Bowman (kjbowman00)
+ */
+package juice;
+
+/**
+ * Runs the game loop
+ *
+ * @author Kyle Bowman (kjbowman00)
+ * @version 04/27/2019
+ */
+public class GameRunner {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
